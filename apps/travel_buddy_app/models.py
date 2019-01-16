61e4258d728c17bbfd85d6fc89300048464de132
GodwinYeboah <godwinyeboah95@gmail.com>
2019-01-16 08:54:56 -0500

    added trip validation and login validation

--- apps/travel_buddy_app/models.py
+++ apps/travel_buddy_app/models.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 from django.contrib import messages
+from django.utils import timezone
 import bcrypt
+
 
 class UserManger(models.Manager):
     pass
@@ -16,23 +18,51 @@ class UserManger(models.Manager):
         }
         # need to greater then 3 
         if len(userData["name"]) < 3: 
-            response["errors"].append('user name must be 3 charater and greater');
-            response['valid'] = False;
+            response["errors"].append('user name must be 3 charater and greater')
+            response['valid'] = False
 
         if len(userData["password"]) < 8: 
-            response["errors"].append('password name must be 8 charater and greater');
-            response['valid'] = False;
+            response["errors"].append('password name must be 8 charater and greater')
+            response['valid'] = False
 
         if len(userData["password"] != userData["confirm_password"]):
-            response['errors'].append('Your password does not match');
-            response['valid'] = False;
-
+            response['errors'].append('Your password does not match')
+            response['valid'] = False
         else:
-            pass
-          
+            if response['valid']:
+                userData.save(
+                    name=userData['name'], 
+                    username=userData['username'], 
+                    password=userData['password']
+                )
 
+
+class TripManger(models.Manager):
     
+    def trip_validation(self, tripData):
+        
+        response = {
+            'errors' : [],
+            'trip': None,
+            'valid': True
+        }
 
+        if len(tripData['destination']) < 0:
+            response['errors'].append("You must add a destination")
+            response['valid'] = False   
+        if len(tripData['description']) < 0:
+            response['errors'].append("You must add a description")
+            response['valid'] = False 
+        else:
+            if response['valid']:
+                tripData.save(
+                    user = tripData['user'], 
+                    destination= tripData['destination'], 
+                    description= tripData['description'], 
+                    travel_date_from = tripData['travel_date_from'], 
+                    travel_date_to = tripData['travel_date_to'], 
+                )
+                
 
 
 # Create your models here.
@@ -42,13 +72,15 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    object  = UserManger()
 
 class Trip(models.Model):
-    trip_by = models.ForeignKey(User, related_name="trip_by_user")
+    user = models.ForeignKey(User)
     destination = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     travel_date_from = models.DateField()
     travel_date_to = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    object  = TripManger()
 
